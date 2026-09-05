d1893feabcb48a09bb83f29b3ff20c97b01f594c
sergioviss <sergio@cuatropuntocero.solutions>
2026-09-04 21:54:51 -0700

    Fix en index de users

--- app/helpers/datatable_helper.rb
+++ app/helpers/datatable_helper.rb
@@ -47,7 +47,14 @@ module DatatableHelper
       export_url,
       class: 'btn btn-success shadow-sm datatable-export-btn'
     ) do
-      raw('<i class="fas fa-file-excel me-2"></i>Exportar a Excel')
+      raw(<<~HTML.squish)
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+          <polyline points="7 10 12 15 17 10"></polyline>
+          <line x1="12" y1="15" x2="12" y2="3"></line>
+        </svg>
+        <span>Exportar a Excel</span>
+      HTML
     end
   end
 
